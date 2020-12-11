1cf5df7a5df69d5876dd4e9f0d6456c7b99e3c03
virtualaizer <pont131995@gmail.com>
2020-12-11 19:22:05 +0500

    update

--- pantext/prod_settings.py
+++ pantext/prod_settings.py
@@ -1,17 +1,8 @@
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '142.93.131.80', 'pantext.ru', 'www.pantext.ru', 'localhost']
